6e8e3ca6827e3f03dddd30036704fca8e39d423d
nanameki1213 <nanameki1213@nanameki1213.github.com>
2023-08-27 01:36:13 +0900

    評価関数vsランダム

--- source/main.cpp
+++ source/main.cpp
@@ -38,45 +38,62 @@ int main(int argc, char *argv[])
     cout << "turn:" << count << endl;
     cout << "current_:" << ((match.next_turn == Player1) ? "Player1" : "Player2") << endl;
 
-    Game_Node *root_node[info->agent]; // ゲーム木の根
+    if(match.next_turn == Player1) {
+      Game_Node *root_node[info->agent]; // ゲーム木の根
 
-    for(int i = 0; i < info->agent; i++) {
-      Field_t **tmp = new Field_t*[info->height]();
-      for(int j = 0; j < info->height; j++) {
-        tmp[j] = new Field_t[info->height]();
-        memcpy(tmp[j], match.map[j], info->height);
-      }
-      Board *init_board = new Board(tmp, info);
-      init_board->next_turn = match.next_turn;
-      root_node[i] = new Game_Node(init_board);
-      cout << "職人" << i << "(" << +root_node[i]->board->agent1[i].x << ", " << +root_node[i]->board->agent1[i].y << ")" << "のゲーム木構築中..." << endl;
-      expandChildren_by_num(root_node[i], 2, i);
-      cout << "職人" << i << "の盤面評価中..." << endl;
-      TreeSearch(root_node[i], i);
-      for(int j = 0; j < info->height; j++) {
-        delete tmp[j];
-      }
-      delete tmp;
-    }
-
-    for(int i = 0; i < info->agent; i++) {
-      cout << "職人" << i << "のスコア: " << root_node[i]->evaluation << endl;
-    }
-
-    // 職人のゲーム木構築
-    cout << "探索終わり\n";
-    for(int i = 0; i < info->agent; i++) {
-      for(int j = 0; j < root_node[i]->childrenNode.size(); j++) {
-        if(root_node[i]->evaluation == root_node[i]->childrenNode[j]->evaluation) {
-          // cout << "j:" << j << endl;
-          root_node[i]->pre_act = root_node[i]->childrenNode[j]->pre_act;
-          // cout << "kind:" << +root_node[i]->childrenNode[j]->pre_act.kind << ", direc:" << +root_node[i]->childrenNode[j]->pre_act.direc << endl;
-          break;
+      for(int i = 0; i < info->agent; i++) {
+        Field_t **tmp = new Field_t*[info->height]();
+        for(int j = 0; j < info->height; j++) {
+          tmp[j] = new Field_t[info->height]();
+          memcpy(tmp[j], match.map[j], info->height);
         }
+        Board *init_board = new Board(tmp, info);
+        init_board->next_turn = match.next_turn;
+        root_node[i] = new Game_Node(init_board);
+        cout << "職人" << i << "(" << +root_node[i]->board->agent1[i].x << ", " << +root_node[i]->board->agent1[i].y << ")" << "のゲーム木構築中..." << endl;
+        expandChildren_by_num(root_node[i], 2, i);
+        cout << "職人" << i << "の盤面評価中..." << endl;
+        TreeSearch(root_node[i], i);
+        for(int j = 0; j < info->height; j++) {
+          delete tmp[j];
+        }
+        delete tmp;
       }
-      match.ActionAnAgent(match.next_turn, i, root_node[i]->pre_act);
-    }
 
+      for(int i = 0; i < info->agent; i++) {
+        cout << "職人" << i << "のスコア: " << root_node[i]->evaluation << endl;
+      }
+
+      // 職人のゲーム木構築
+      cout << "探索終わり\n";
+      for(int i = 0; i < info->agent; i++) {
+        for(int j = 0; j < root_node[i]->childrenNode.size(); j++) {
+          if(root_node[i]->evaluation == root_node[i]->childrenNode[j]->evaluation) {
+            // cout << "j:" << j << endl;
+            root_node[i]->pre_act = root_node[i]->childrenNode[j]->pre_act;
+            // cout << "kind:" << +root_node[i]->childrenNode[j]->pre_act.kind << ", direc:" << +root_node[i]->childrenNode[j]->pre_act.direc << endl;
+            break;
+          }
+        }
+        match.ActionAnAgent(match.next_turn, i, root_node[i]->pre_act);
+      }
+    } else {
+      Action *act;
+      act = new Action[info->agent];
+      for(int i = 0; i < info->agent; i++) {
+        vector<Action> legal_act;
+        match.getLegalAct(match.next_turn, legal_act, i);
+        int rand_act = rand()%legal_act.size();
+        act[i] = legal_act[rand_act];
+        // cout << "select: " << (int)act[i].kind << ", " << (int)act[i].direc << endl;
+        match.ActionAnAgent(match.next_turn, i, act[i]);
+
+      // 陣地ができたかどうかを確認し，更新する
+      // game.Encamp_Update();
+
+      uint8_t x, y;
+      }
+    }
     match.next_turn = !match.next_turn;
 
   }
