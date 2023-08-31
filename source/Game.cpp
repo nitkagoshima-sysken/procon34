b3d21394fbfeaf1726388bc26bfa4d0e7b27b5dc
nanameki1213 <nanameki1213@nanameki1213.github.com>
2023-09-01 00:08:55 +0900

    バグ修正(おわり)

--- source/Game.cpp
+++ source/Game.cpp
@@ -42,7 +42,7 @@ Board::Board(const Board &board)
   map = new Bitmap_t*[board.info->height]();
   for(int i = 0; i < board.info->height; i++) {
     map[i] = new Bitmap_t[board.info->width]();
-    memcpy(map[i], board.map[i], board.info->width);
+    memcpy(map[i], board.map[i], sizeof(Bitmap_t) * board.info->width);
   }
   
   info = new FieldInfo;
