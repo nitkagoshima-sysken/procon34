e7fc27b20507024427a14778216bf720882b03d2
mizukaki <mizukaki3202@gmail.com>
2023-09-02 18:34:03 +0900

    diffスコアの実装

--- source/Evaluation_functions.cpp
+++ source/Evaluation_functions.cpp
@@ -4,11 +4,19 @@
 #include <vector>
 
 // 両者の点数の差だけをスコアとして返却する評価関数
-static int depth_one(Board *board) {
-  
+static int ev_diff_score(Board *board) {
+    //自分と相手のスコアを取得
+    int score1, score2;
+  board->score(score1, score2);
+
+  return (score2 - score1);
 }
 
 // 敵の壁の数をスコア換算する評価関数
-static int destroy(Board *board) {
-
+static int ev_destroy(Board *board) {
+int score1, score2;
+  board->score(score1, score2);
+//合法手の数
+std::vector<Action> act;
+board->getLegalAct(Player2, act, )
 }
