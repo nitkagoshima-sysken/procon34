69f94797f3373982b086325cdf51ed94de7cbd85
nanameki1213 <nanameki1213@nanameki1213.github.com>
2023-09-02 16:35:58 +0900

    Evaluation_function.cpp追加

---
+++ source/Evaluation_functions.cpp
@@ -0,0 +1,14 @@
+#include "procon2023.hpp"
+#include "Game.hpp"
+#include "Game_Node.hpp"
+#include <vector>
+
+// 両者の点数の差だけをスコアとして返却する評価関数
+static int depth_one(Board *board) {
+  
+}
+
+// 敵の壁の数をスコア換算する評価関数
+static int destroy(Board *board) {
+
+}
