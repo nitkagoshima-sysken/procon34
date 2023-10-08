ebe875e6d02b5f5fa1268070176b03967a470b59
nanameki1213 <nanameki1213@nanameki1213.github.com>
2023-10-09 04:18:44 +0900

    こまかな修正

--- network_interface/terminal.py
+++ network_interface/terminal.py
@@ -11,36 +11,10 @@ path = "/matches/10"
 file = "res.json"
 old_file = "res.json.old"
 
-turn = 0
-
-# post_num = 0
-
 class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        with open(file, 'r') as f:
-            data = f.read()
-
-        global turn
-        jobj = json.loads(data)
-        get_turn = jobj['turn']
-        print('turn:' + str(turn) + ', get_turn: ' + str(get_turn))
-
-        # 取得したターン数が現在の次のターンになっているかどうか
-        if turn != get_turn:
-            print('failed')
-            self.wfile.write('no changes'.encode())
-        else:
-            print('success')
-            self.wfile.write(data.encode())
-
-        # 現在のターン数を更新
-        turn += 2
-
     def do_POST(self):
+        '''APP SERVERから行動計画がPOSTされる'''
         data = self.rfile.read(int(self.headers['content-length'])).decode('utf-8')
 
         HEADER = {
@@ -48,15 +22,12 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
             'Content-Length': self.headers['content-length']
         }
 
-        global turn
-
         print(data)
         data_encode = json.dumps(json.loads(data))
         print(data_encode)
-        print(turn)
         res = requests.post(HOST + path + '?token=' + TOKEN, headers=HEADER, data=data_encode)
         print(res.status_code)
-        turn += 2
+        
         self.send_response(res.status_code)
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.end_headers()
